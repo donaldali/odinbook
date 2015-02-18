9f10fd40c5d8e058480581f714dfe36e1cbb0c1e
Donald Ali <donaldali@yahoo.com>
2015-02-17 21:09:36 -0800

    Create Grid and Style Sidebar
    Create grid for views and a nested layout for Users (to be used for
    views of a signed in user.)
    Modify Notifications controller and various previously created views
    to use the new Users layout.
    Create and style the sidebar used in the grid.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  layout "layouts/users"
+
   def index
     @notifications = current_user.notifications
     current_user.reset_new_notifications
